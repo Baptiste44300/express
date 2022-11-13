7c8bc67ecf1c32a8af101575b27d888adde5e340
Baptiste44300 <baudouard44300@gmail.com>
2022-11-13 22:32:48 +0100

    oodz

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 require("dotenv").config();
-
 const express = require("express");
 
 const app = express();
 
-const port = process.env.APP_PORT ?? 5003;
+const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+  res.send("Welcome to my list of Users");
 };
 
 app.get("/", welcome);
 
-const movieHandlers = require("./movieHandlers");
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
 
-app.get("/api/users", movieHandlers.getMovies);
-app.get("/api/users/:id", movieHandlers.getMovieById);
+app.post("/api/movies", movieHandlers.postMovie);
+
+app.post("/api/users", users.postUsers);
+
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+
+app.put("/api/users/:id", users.updateUsers);
+
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
+app.delete("/api/movies/:id", users.deleteUser);
+
+
+const users = require("./users");
+
+app.get("/api/users", users.getUsers);
+app.get("/api/users/:id", users.getUsersById);
 
 app.listen(port, (err) => {
   if (err) {
--- movieHandlers.js
+++ movieHandlers.js
@@ -25,14 +25,47 @@ const movies = [
   },
 ];
 
+
 const database = require("./database");
 
 const getMovies = (req, res) => {
+  let sql = "select * from movies";
+  const sqlValues = [];
+
+  if (req.query.color != null) {
+    where.push({
+      column: "color",
+      value: req.query.color,
+      operator: "=",
+    });
+  }
+  if (req.query.max_duration != null) {
+    where.push({
+      column: "duration",
+      value: req.query.max_duration,
+      operator: "<=",
+    });
+  }
+
   database
-    .query("select * from movies")
-    .then(([movies]) => {
-      res.json(movies);
-    })
+
+  .query(
+    where.reduce(
+      (sql, { column, operator }, index) =>
+        `${sql} ${index === 0 ? "where" : "and"} ${column} ${operator} ?`,
+      initialSql
+    ),
+    where.map(({ value }) => value)
+  )
+  .then(([movies]) => {
+    res.json(movies);
+  })
+
+    // .query("select * from movies")
+    // .then(([movies]) => {
+    //   res.json(movies);
+    // })
+
     .catch((err) => {
       console.error(err);
       res.status(500).send("Error retrieving data from database");
@@ -57,11 +90,75 @@ const getMovieById = (req, res) => {
     });
 };
 
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
 
+  database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the movie");
+    });
+};
+
+const updateMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "update movies set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?",
+      [title, director, year, color, duration, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error editing the movie");
+    });
+};
+
+const deleteMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from movies where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the movie");
+    });
+};
 
 module.exports = {
   getMovies,
   getMovieById,
+  postMovie, 
+  updateMovie,
+  deleteMovie
 };
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+
 
 
